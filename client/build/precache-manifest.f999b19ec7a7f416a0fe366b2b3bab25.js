72f9c3399bf1e3db770bf7ad7e0aec07892545bd
Jeffe <j.garner200@gmail.com>
2020-10-01 13:10:14 -0400

    -

---
+++ client/build/precache-manifest.f999b19ec7a7f416a0fe366b2b3bab25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4faf8ae17d826401028e8e8e80e45ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6415ae3927a5a630c01a",
+    "url": "/static/css/main.80cd92d2.chunk.css"
+  },
+  {
+    "revision": "3aee066b582b8961f6fd",
+    "url": "/static/js/2.1ed076b3.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.1ed076b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6415ae3927a5a630c01a",
+    "url": "/static/js/main.b66714e2.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
